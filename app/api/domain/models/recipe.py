3eaa5260c77c00e375585251571db30b6d3cfb0f
David Pinheiro <dkspinheiro@gmail.com>
2024-07-14 21:36:16 -0300

    adjust the domain model

--- app/api/domain/models/recipe.py
+++ app/api/domain/models/recipe.py
@@ -1,15 +1,19 @@
 from dataclasses import dataclass
 from datetime import datetime
+from typing import Optional
 
 from dataclasses_json import dataclass_json, LetterCase
+from pydantic import BaseModel
 
 
 @dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass(frozen=True)
-class Recipe:
-    id = int
+class Recipe(BaseModel):
+    id: Optional[int] = None
     title = str
-    making_time = datetime
+    making_time = str
     serves = str
     ingredients = str
     cost = int
+    created_at: Optional[datetime] = None
+    update_at: Optional[datetime] = None
